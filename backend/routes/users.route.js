1f70bc700a7dae1b03722131bf92321c6d6eb97a
dverma321 <151020798+dverma321@users.noreply.github.com>
2024-04-24 17:39:15 +0530

    Update users.route.js

--- backend/routes/users.route.js
+++ backend/routes/users.route.js
@@ -9,6 +9,14 @@ const route = express.Router();
 
 route.use(cookieParser()); // Use cookie-parser middleware first to parse cookies
 
+// CORS configuration
+route.use(cors({
+    origin: "https://globalchatting.netlify.app",
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true, // Set credentials to true for cookies
+    allowedHeaders: 'Authorization, Content-Type', // Include necessary headers
+}));
+
 route.get("/", protectRoute, usersToSideBar);
 
 module.exports = route;
