46a2e31601d8cadfe71238911726dd518f6e0018
dverma321 <151020798+dverma321@users.noreply.github.com>
2024-04-24 17:39:49 +0530

    Update messages.route.js

--- backend/routes/messages.route.js
+++ backend/routes/messages.route.js
@@ -10,6 +10,14 @@ const { messageRoutes, getMessages  } = require('../controllers/message.controll
 const router = express.Router();
 router.use(cookieParser()); // Use cookie-parser middleware first to parse cookies
 
+// CORS configuration
+route.use(cors({
+    origin: "https://globalchatting.netlify.app",
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true, // Set credentials to true for cookies
+    allowedHeaders: 'Authorization, Content-Type', // Include necessary headers
+}));
+
 router.post("/send/:id", protectRoute, messageRoutes); // you have to import the same name like messageRoutes in your controller
 router.get("/:id", protectRoute, getMessages); 
 
