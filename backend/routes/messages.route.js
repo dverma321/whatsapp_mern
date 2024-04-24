647fd89a6d876ccfe5538d9dcfe9e3ad60832100
dverma321 <151020798+dverma321@users.noreply.github.com>
2024-04-24 17:30:36 +0530

    Update messages.route.js

--- backend/routes/messages.route.js
+++ backend/routes/messages.route.js
@@ -1,12 +1,14 @@
 const express = require('express');
 const dotenv = require('dotenv');
 const protectRoute = require('../middleware/protectRoute.js');
+const cookieParser = require('cookie-parser');
 
 dotenv.config();
 
 const { messageRoutes, getMessages  } = require('../controllers/message.controller.js');
 
 const router = express.Router();
+route.use(cookieParser()); // Use cookie-parser middleware first to parse cookies
 
 router.post("/send/:id", protectRoute, messageRoutes); // you have to import the same name like messageRoutes in your controller
 router.get("/:id", protectRoute, getMessages); 
