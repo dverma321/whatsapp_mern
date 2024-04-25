80b266d46eb6339de0a89e5b349f44e2588ee5ff
dverma321 <151020798+dverma321@users.noreply.github.com>
2024-04-25 15:58:36 +0530

    Update socket.js

--- backend/socket/socket.js
+++ backend/socket/socket.js
@@ -6,12 +6,13 @@ const app = express();
 
 const server = http.createServer(app);
 const io = new Server(server, {
-    cors: {
-        origin: "http://localhost:5173",
-        methods: ['GET', 'POST'],
-        credentials: true
-    }
-});
+   app.use(cors({
+    origin: "https://globalchatting.netlify.app",
+    methods: ['GET', 'HEAD', 'PUT', 'PATCH', 'POST', 'DELETE'],
+    credentials: true,
+    optionsSuccessStatus: 204
+}));
+
 
 
 const userSocketMap = {}; // {userId: socket.id}
