1043b19e5610238b1be3a59ce536f206e81d5ead
dverma321 <151020798+dverma321@users.noreply.github.com>
2024-04-25 09:41:54 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -1,42 +1,33 @@
-const express = require('express')
+const express = require('express');
 const dotenv = require('dotenv');
 dotenv.config();
 
 const cookieParser = require('cookie-parser');
+const cors = require('cors');
 
 const useRouths = require('../backend/routes/auth.route.js');
 const messageRouths = require('../backend/routes/messages.route.js');
 const usersRouths = require('../backend/routes/users.route.js');
 const connectToMongodb = require('./db/mongooseConnect.js');
-const cors = require('cors');
 const { app, server } = require('./socket/socket.js');
 
 const PORT = process.env.PORT || 5000;
 
-// const app = express();
-
-app.use(express.json()) // accepting request from req.body or frontend in the form of json payload, it's the middleware
+app.use(express.json());
 app.use(cookieParser());
+app.use(cors({
+    origin: "https://globalchatting.netlify.app",
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true,
+    optionsSuccessStatus: 204,
+    allowedHeaders: 'Authorization, Content-Type',
+}));
 
-app.use(cors(
-    {
-        origin: "https://globalchatting.netlify.app",
-        // origin:"https://findyourperfectmatch.netlify.app",
-        methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-        credentials: true, // set the cookie true
-        optionsSuccessStatus: 204,     // Respond with a 204 status code for preflight requests
-        allowedHeaders: 'Authorization, Content-Type', // Add required headers
-    }
-));
-
-
-app.use('/api/auth', useRouths); // all routes for login, signup, logout
-app.use('/api/messages', messageRouths); // all routes for messages
-app.use('/api/users', usersRouths); // all users route except current user
+app.use('/api/auth', useRouths);
+app.use('/api/messages', messageRouths);
+app.use('/api/users', usersRouths);
 
 server.listen(PORT, () => {
-    connectToMongodb(); // connecting the mongodb database
-    console.log(`server is Running on port number ${PORT}`)
-
-}
-)
+    connectToMongodb();
+    console.log(`server is Running on port number ${PORT}`);
+});
