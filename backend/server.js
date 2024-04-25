06597ed2e412358aa652de7f9110e471b3f29b02
dverma321 <151020798+dverma321@users.noreply.github.com>
2024-04-25 15:23:00 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -15,19 +15,17 @@ const PORT = process.env.PORT || 5000;
 
 // const app = express();
 
+
+app.use(cors({
+    origin: "https://globalchatting.netlify.app",
+    methods: ['GET', 'HEAD', 'PUT', 'PATCH', 'POST', 'DELETE'],
+    credentials: true,
+    optionsSuccessStatus: 204
+}));
+
 app.use(express.json()) // accepting request from req.body or frontend in the form of json payload, it's the middleware
+
 app.use(cookieParser());
-
-app.use(cors(
-    {
-        origin: "http://localhost:5173",
-        // origin:"https://findyourperfectmatch.netlify.app",
-        methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-        credentials: true, // set the cookie true
-        optionsSuccessStatus: 204     // Respond with a 204 status code for preflight requests
-    }
-));
-
 
 app.use('/api/auth', useRouths); // all routes for login, signup, logout
 app.use('/api/messages', messageRouths); // all routes for messages
