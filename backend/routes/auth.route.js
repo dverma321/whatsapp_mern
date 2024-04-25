c07eef2b4888412ebce835791aa172c1e815764a
dverma321 <151020798+dverma321@users.noreply.github.com>
2024-04-25 14:02:07 +0530

    Update auth.route.js

--- backend/routes/auth.route.js
+++ backend/routes/auth.route.js
@@ -7,14 +7,6 @@ const { loginUser, signupUser, logoutUser } = require('../controllers/auth.contr
 
 const router = express.Router();
 
-router.use(cors({
-    origin: "https://globalchatting.netlify.app",
-    methods: ['GET', 'HEAD', 'PUT', 'PATCH', 'POST', 'DELETE'],
-    credentials: true,
-    optionsSuccessStatus: 204,
-    allowedHeaders: ['Authorization', 'Content-Type', 'Access-Control-Allow-Origin'],
-}));
-
 router.post('/signup', signupUser);
 
 router.post('/login', loginUser);
