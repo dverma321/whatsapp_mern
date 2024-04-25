519b8fa6406300903f6d11fcd687c9518cc63aa5
dverma321 <151020798+dverma321@users.noreply.github.com>
2024-04-25 11:53:14 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -15,13 +15,16 @@ const PORT = process.env.PORT || 5000;
 
 app.use(express.json());
 app.use(cookieParser());
+
 app.use(cors({
     origin: "https://globalchatting.netlify.app",
-    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    methods: ['GET', 'HEAD', 'PUT', 'PATCH', 'POST', 'DELETE'],
     credentials: true,
     optionsSuccessStatus: 204,
-    allowedHeaders: 'Authorization, Content-Type, Access-Control-Allow-Origin',
+    allowedHeaders: ['Authorization', 'Content-Type', 'Access-Control-Allow-Origin'],
 }));
+
+
 
 app.use('/api/auth', useRouths);
 app.use('/api/messages', messageRouths);
