ceed1d109a4404ae2c13303bbb8ad77562da0727
dverma321 <151020798+dverma321@users.noreply.github.com>
2024-04-25 15:07:37 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -14,7 +14,6 @@ const { app, server } = require('./socket/socket.js');
 const PORT = process.env.PORT || 5000;
 
 app.use(express.json());
-app.use(cookieParser());
 
 app.use(cors({
     origin: "https://globalchatting.netlify.app",
@@ -23,7 +22,7 @@ app.use(cors({
     optionsSuccessStatus: 204
 }));
 
-
+app.use(cookieParser());
 
 app.use('/api/auth', useRouths);
 app.use('/api/messages', messageRouths);
